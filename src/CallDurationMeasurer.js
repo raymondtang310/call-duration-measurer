1d0296d24072346c7d9176c5d5273cc31fea13c8
Raymond Tang <raymond.tang@libertymutual.com>
2020-12-11 16:46:11 -0500

    feat: Add abilities to invoke calls and retrieve call durations

--- src/CallDurationMeasurer.js
+++ src/CallDurationMeasurer.js
@@ -1,8 +1,55 @@
 class CallDurationMeasurer {
-  // This function is temporary and will be removed.
-  // This is only here currently for the purpose of getting the structure of this codebase in place.
-  placeholder() {
-    return 'placeholder';
+  callDurations = [];
+
+  /**
+   * Invokes the given callback and records the duration of the call.
+   *
+   * If the result of the callback is a promise, then:
+   * 1. the time taken for the promise to resolve is included in the recorded duration
+   * 2. a promise is returned containing the resolved value of the promise returned by the callback
+   *
+   * Otherwise:
+   * 1. the time taken for the callback to run is recorded
+   * 2. the result of the callback is returned
+   *
+   * @param {Function} callback - The function to invoke
+   * @param {Object} scope - The value of the "this" keyword provided for the call to the given callback
+   * @param {...*} [args] - Arguments with which the given callback should be called
+   * @returns {*} If the result of the callback is a promise, returns a promise containing the resolved value of the
+   *              promise returned by the callback. Otherwise, returns the result of the callback.
+   */
+  invoke(callback, scope, ...args) {
+    const startTime = new Date();
+    let callbackResult;
+
+    if (scope) {
+      callbackResult = callback.apply(scope, args);
+    } else {
+      callbackResult = callback(...args);
+    }
+
+    const recordCallbackDuration = data => {
+      const endTime = new Date();
+      this.callDurations.push({
+        name: callback.name,
+        duration: endTime - startTime,
+      });
+
+      return data;
+    };
+
+    return callbackResult instanceof Promise
+      ? callbackResult.then(recordCallbackDuration)
+      : recordCallbackDuration(callbackResult);
+  }
+
+  /**
+   * Returns all call durations measured by this {@link CallDurationMeasurer}.
+   *
+   * @returns {[]} Returns all call durations measured by this {@link CallDurationMeasurer}.
+   */
+  getCallDurations() {
+    return this.callDurations;
   }
 }
 
--- src/__tests__/CallDurationMeasurer.test.js
+++ src/__tests__/CallDurationMeasurer.test.js
@@ -1,13 +1,141 @@
 import CallDurationMeasurer from '../CallDurationMeasurer';
 
 describe('CallDurationMeasurer', () => {
-  describe('placeholder', () => {
-    it('should return "placeholder"', () => {
-      const callDurationMeasurer = new CallDurationMeasurer();
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
 
-      const result = callDurationMeasurer.placeholder();
+  describe('invoke', () => {
+    describe('when there is a given scope', () => {
+      it('should invoke the given callback with the given arguments using the given scope', () => {
+        const callDurationMeasurer = new CallDurationMeasurer();
+        const scope = { callback: jest.fn().mockName('callback') };
+        const args = ['arg'];
 
-      expect(result).toEqual('placeholder');
+        scope.callback.apply = jest.fn().mockName('callback.apply');
+
+        callDurationMeasurer.invoke(scope.callback, scope, ...args);
+
+        expect(scope.callback.apply).toHaveBeenCalledTimes(1);
+        expect(scope.callback.apply).toHaveBeenCalledWith(scope, args);
+      });
+    });
+
+    describe('when there is NOT a given scope', () => {
+      it('should invoke the given callback with the given arguments', () => {
+        const callDurationMeasurer = new CallDurationMeasurer();
+        const callback = jest.fn().mockName('callback');
+        const args = ['arg'];
+
+        callDurationMeasurer.invoke(callback, null, ...args);
+
+        expect(callback).toHaveBeenCalledTimes(1);
+        expect(callback).toHaveBeenCalledWith(...args);
+      });
+    });
+
+    describe('when the given callback has finished executing', () => {
+      describe('and the callback returns a promise', () => {
+        it('should return a promise containing the resolved value of the promise returned by the callback', async () => {
+          const callDurationMeasurer = new CallDurationMeasurer();
+          const resolvedValue = 'resolvedValue';
+          const scope = {
+            // We need to use mockReturnValue here with a resolved promise rather than mockResolvedValue due
+            // to the "instanceof Promise" check used in the implementation.
+            callback: jest.fn().mockName('callback').mockReturnValue(Promise.resolve(resolvedValue)),
+          };
+
+          const result = await callDurationMeasurer.invoke(scope.callback, scope);
+
+          expect(result).toEqual(resolvedValue);
+        });
+      });
+
+      describe('and the callback does NOT return a promise', () => {
+        it('should return the result of the callback', () => {
+          const callDurationMeasurer = new CallDurationMeasurer();
+          const callbackReturnValue = 'hello';
+          const scope = {
+            callback: jest.fn().mockName('callback').mockReturnValue(callbackReturnValue),
+          };
+
+          const result = callDurationMeasurer.invoke(scope.callback, scope);
+
+          expect(result).toEqual(callbackReturnValue);
+        });
+      });
+    });
+  });
+
+  describe('getCallDurations', () => {
+    describe('when 0 calls have been invoked', () => {
+      it('should return an empty array', () => {
+        const callDurationMeasurer = new CallDurationMeasurer();
+
+        const result = callDurationMeasurer.getCallDurations();
+
+        expect(result).toEqual([]);
+      });
+    });
+
+    describe('when 1 call has been invoked', () => {
+      it('should return an array containing the call duration for that call', () => {
+        const callDurationMeasurer = new CallDurationMeasurer();
+        const callback = () => {};
+        const mockStartTime = new Date(0);
+        const mockEndTime = new Date(100);
+        const dateSpy = jest
+          .spyOn(global, 'Date')
+          .mockImplementationOnce(() => mockStartTime)
+          .mockImplementationOnce(() => mockEndTime);
+
+        callDurationMeasurer.invoke(callback);
+        const result = callDurationMeasurer.getCallDurations();
+
+        expect(result).toEqual([
+          {
+            name: 'callback',
+            duration: 100,
+          },
+        ]);
+
+        dateSpy.mockRestore();
+      });
+    });
+
+    describe('when multiple calls have been invoked', () => {
+      it('should return an array containing the call durations for every call', () => {
+        const callDurationMeasurer = new CallDurationMeasurer();
+        const callback1 = () => {};
+        const callback2 = () => {};
+        const mockCallback1StartTime = new Date(0);
+        const mockCallback1EndTime = new Date(100);
+        const mockCallback2StartTime = new Date(100);
+        const mockCallback2EndTime = new Date(300);
+        const dateSpy = jest
+          .spyOn(global, 'Date')
+          .mockImplementationOnce(() => mockCallback1StartTime)
+          .mockImplementationOnce(() => mockCallback1EndTime)
+          .mockImplementationOnce(() => mockCallback2StartTime)
+          .mockImplementationOnce(() => mockCallback2EndTime);
+
+        callDurationMeasurer.invoke(callback1);
+        callDurationMeasurer.invoke(callback2);
+        const result = callDurationMeasurer.getCallDurations();
+
+        expect(result).toEqual([
+          {
+            name: 'callback1',
+            duration: 100,
+          },
+          {
+            name: 'callback2',
+            duration: 200,
+          },
+        ]);
+
+        dateSpy.mockRestore();
+      });
     });
   });
 });
